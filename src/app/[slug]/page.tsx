0af7634a158d909fdcb662524eb2b009439a7c10
Mehmet <mehmetext68@gmail.com>
2025-06-26 17:46:40 +0300

    Update page.tsx

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,12 @@
-export default function PostPage({ params }: { params: { slug: string } }) {
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
   return (
     <main className="container py-10 flex flex-col gap-4">
-      <h1 className="text-4xl font-bold">PostPage {params.slug}</h1>
+      <h1 className="text-4xl font-bold">PostPage {slug}</h1>
     </main>
   );
 }
