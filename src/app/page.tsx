bb370197a2a0c77991fde95d21ee08ebd23fd0d6
Mehmet <mehmetext68@gmail.com>
2025-06-26 17:45:13 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="container py-10 flex flex-col gap-4">
       <h1 className="text-4xl font-bold">Son Yazılar</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <PostItem />
         <PostItem />
         <PostItem />
